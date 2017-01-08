f9556baa34f9590073b9df3cb9036414fc0a2183
itottm <rhcp2106@fork.ocn.ne.jp>
2017-01-08 19:05:00 +0900

    topicのindexアクションにinfinitScrollを実装

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,11 @@ class Topic < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   validates :content, presence: true
+
+  paginates_per 10
+
+  scope :index_all, -> {
+    select(:id, :content, :user_id, :created_at)
+        .order(created_at: :desc)
+  }
 end
