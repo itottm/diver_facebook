a4d10fbc1f1578f7f11271077cf0303eab9561bb
itottm <rhcp2106@fork.ocn.ne.jp>
2017-01-20 02:24:04 +0900

    本番環境用SendGrid設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://52.192.157.214' }
+  config.action_mailer.default_url_options = { host: 'http://52.197.91.179' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
       {
           user_name: ENV['SENDGRID_USERNAME'],
           password: ENV['SENDGRID_PASSWORD'],
-          domain: '52.192.157.214',
+          domain: '52.197.91.179',
           address: 'smtp.sendgrid.net',
           port: 587,
           authentication: :plain,
