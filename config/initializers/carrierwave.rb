654a36575b3524ac07afb86cefb3f2de067be375
伊藤 <rhcp2106@fork.ocn.ne.jp>
2017-01-22 14:20:42 +0900

    carrierwaveとS3連携設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,10 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'diver-facebook-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/diver-facebook-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.fog_directory = 'diver-facebook-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/diver-facebook-development'
   end
 end
