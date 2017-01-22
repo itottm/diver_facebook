f901fb5731bd08f9a36b229fdc5db776149d22bb
伊藤 <rhcp2106@fork.ocn.ne.jp>
2017-01-22 19:39:48 +0900

    seeds.rbにコメント用のダミーデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,32 @@
-100.times do |n|
+50.times do |n|
   email = Faker::Internet.email
   name = Faker::Name.name
   password = "password"
-  User.create!(email: email,
-               name: name,
-               password: password,
-               password_confirmation: password,
-               uid: n
+  User.create!(
+      email: email,
+      name: name,
+      password: password,
+      password_confirmation: password,
+      uid: n
   )
 end
 
 100.times do |n|
   content = Faker::Lorem.paragraphs
-  user_id = Faker::Number.between(1, 100)
+  user_id = Faker::Number.between(1, 50)
   Topic.create!(
-               content: content,
-               user_id: user_id
+      content: content,
+      user_id: user_id
+  )
+end
+
+100.times do |n|
+  content = Faker::Lorem.paragraphs
+  user_id = Faker::Number.between(1, 50)
+  topic_id = Faker::Number.between(1, 100)
+  Comment.create!(
+      content: content,
+      user_id: user_id,
+      topic_id: topic_id
   )
 end
